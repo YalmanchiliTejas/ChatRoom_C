cd46de4eeb211ac300449f20db0cc8a1d820d585
YalmanchiliTejas <85045556+YalmanchiliTejas@users.noreply.github.com>
2022-05-27 20:48:01 +0530

    added some more parameters

--- student.h
+++ student.h
@@ -1,14 +1,21 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+
+#include <sys/malloc.h>
+
 #define MAX_PASS_LEN (9)
 #define ERROR_CODE_STU (-1)
-
+#define STUDENT_EXISTS (1);
+#define NO_STUDENT (0)
 typedef struct student{
 
 int id;
 char * username;
-char [MAX_PASS_LEN + 1] password;
+char  password [MAX_PASS_LEN];
+char * major;
+struct student * next;
+struct student * prev;
 } student_t;
 #endif // STUDENT_H
 
